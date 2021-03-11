6c7dda1ce1067249c0cf6dc3e30b57667152cc6c
Gen Kasaneiwa <gen@gnw.jp>
2021-03-11 23:56:59 +0900

    test: add

--- sw.js
+++ sw.js
@@ -3,14 +3,40 @@
 //サイトのhostが、httpやipadreess直指定の場合はserviceWorkerがいなくなります。
 //localhostの場合は、httpでもよい。
 
-// service-worker.js
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-  });
-  
-  self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-  });
-  
-  // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-  self.addEventListener('fetch', function(event) {});
+var CACHE_NAME = 'cache-v1';
+//インストール
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {   
+        return cache.addAll('./*');
+    })
+  );
+  console.log('[ServiceWorker] Install');
+});
+
+//キャッシュ比較
+self.addEventListener('activate', function(event) {  
+  event.waitUntil(  
+    caches.keys().then(function(cache) {
+      cache.map(function(name) {
+        if(CACHE_NAME !== name) caches.delete(name);
+      })
+    })  
+  );
+  console.log('[ServiceWorker] Activate');
+});  
+
+//オフライン有効化
+self.addEventListener('fetch', function(event) {
+  event.respondWith( 
+    caches.match(event.request).then(function(res) {
+        if(res) return res;
+      
+        return fetch(event.request);
+    }) 
+  );
+});
+
+  // // 現状では、この処理を書かないとService Workerが有効と判定されないようです
+  // self.addEventListener('fetch', function(event) {});
